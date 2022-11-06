1865f736bdce63ecc307495321c9428b81b66ef7
Ben Selby <benmatselby@gmail.com>
2022-11-06 19:49:54 +0000

    Remove redundant use statement.

--- src/Command/ListPipelinesCommand.php
+++ src/Command/ListPipelinesCommand.php
@@ -8,7 +8,6 @@ namespace Ringli\Command;
 
 use Ringli\Client;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\Helper;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
